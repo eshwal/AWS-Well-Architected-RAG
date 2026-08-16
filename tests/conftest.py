3bbc9447ad9b5b1428479ce7bb06f7d1428ebb38
eshwal <eshaagarwal05071998@gmail.com>
2026-08-16 12:08:41 +0530

    Set correct environment variables

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,7 +18,7 @@ os.environ.setdefault("DEEPEVAL_PER_TASK_TIMEOUT_SECONDS_OVERRIDE","1200")
 os.environ.setdefault("DEEPEVAL_RETRY_MAX_ATTEMPTS","3")
 os.environ.setdefault("DEEPEVAL_RETRY_CAP_SECONDS","60")     # max wait between retries
 os.environ.setdefault("DEEPEVAL_RETRY_INITIAL_SECONDS","5")   # initial backoff
-os.environ.setdefault("LANGCHAIN_TRACING_V2",True)
+os.environ.setdefault("LANGCHAIN_TRACING_V2","true")
 os.environ.setdefault("LANGCHAIN_API_KEY","test")
 os.environ.setdefault("LANGCHAIN_PROJECT","test")
 
